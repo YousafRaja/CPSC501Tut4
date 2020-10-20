b92dcdd5f7f83cfd1fa93c20cc378d6d184ef716
Yousaf <waynestudent38@gmail.com>
2020-10-20 17:13:41 -0600

    array reflection

---
+++ testProjectA/reflection/tutSlides2/ClassA3.java
@@ -0,0 +1,8 @@
+package tutSlides2;
+
+
+public final class ClassA3  {
+    
+    private int[] myNums = new int[] {1, 1, 2, 3, 5, 8};
+
+}
--- testProjectA/reflection/tutSlides2/Driver.java
+++ testProjectA/reflection/tutSlides2/Driver.java
@@ -1,11 +1,92 @@
+
+/*
+ * Exercise (Optional): Loading an array.
+ * 
+ * Fill in the missing code snippets.
+ * The code below should take in an object of type ClassA3 and introspect the myNums integer array. 
+ * 
+ * Time: 10-15 mins
+ * 
+ *  
+ public class Driver {
+	
+	public static void inspectObj(Object obj ){
+		// ...				
+	}
+	
+	public static void inspectField(Field f, Object obj, int depth){
+		// ... 			
+	}
+	
+	
+	public static void main(String[] args){		
+		ClassA myClass = new ClassA();
+		inspectObj(myClass);
+	}
+}
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package tutSlides2;
 
-public class Driver {
-	static char indentChar = '\t';
-    public static void display(int depth, String cname, String info, String fmt) {
-    	String align = "";
-    	for(int i = 0; i<depth; i++)align+=indentChar;
-    	System.out.format(fmt,align,cname,info);
-    }
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
+import java.util.Arrays;
 
+public class Driver {
+
+	static char indentChar = '\t';
+	
+	
+	public static void inspectObj(Object obj ) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		for (Field field : obj.getClass().getDeclaredFields()) {
+			field.setAccessible(true);
+			inspectField(field, obj, 0);
+		}									
+	}
+	
+	private static void inspectField(Field f, Object obj, int depth) throws IllegalArgumentException, IllegalAccessException {
+        Object fval = f.get(obj);
+        if (fval == null) return;
+        if (fval.getClass().isArray()){
+            Object[] arr = null;
+            if (fval.getClass().getComponentType().isPrimitive()) {
+                arr = new Object[Array.getLength(fval)];
+                for(int i = 0; i < arr.length; ++i)
+                    arr[i] = Array.get(fval, i);
+            } else arr = (Object[]) fval;
+            String tabs = "";
+            for(int i = 0; i<depth; i++)tabs += indentChar;
+            System.out.println(MessageFormat.format("array\n{0}\n{1}\n{2}", tabs+"Length: "+arr.length, tabs+"Component Type: "+f.getType().getTypeName(), tabs+"Array Values:"+Arrays.toString(arr))); 
+        }
+	}
+	
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {		
+		ClassA3 myClass = new ClassA3();
+		inspectObj(myClass);
+	}
+	
+	/*
+	 *  Notes: 
+	 *  - Remember that your initial object could also be an array.   
+	 *  
+	 */
+	
 }
+
